e853e2281ff2e99009e186dcb2f3830c59731d5a
mattishenner <118067835+mattishenner@users.noreply.github.com>
2023-03-03 11:14:11 +0100

    Cleaned up code and added icon animation

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+/* ********************** | DO STUFF AFTER LOADING ANIMATION | ********************** */
+/* ********************** | DO STUFF AFTER LOADING ANIMATION | ********************** */
 document.getElementById("load-background").addEventListener("animationend", () =>{
     document.getElementById("load-background").remove();
+    document.querySelector("nav").style.position = "sticky"; 
+    document.body.classList.remove("prevent-scroll");
 });
 
+/* ********************** | ACTION FOR BOOKING BUTTON | ********************** */
+/* ********************** | ACTION FOR BOOKING BUTTON | ********************** */
 const bookButton = document.getElementById("book-button");
-
-document.querySelector("nav").addEventListener("animationend", () =>{
-    document.querySelector("nav").style.position = "sticky";
-});
-
 bookButton.addEventListener("click", () => {
     window.open("https://booking.a-ansigt.dk/", "_blank")
 })
 
+/* ********************** | RESPONSIVE MENU | ********************** */
+/* ********************** | RESPONSIVE MENU | ********************** */
 const menuIcon = document.getElementById("menu-icon");
 const navList = document.getElementById("nav-list");
-
 menuIcon.addEventListener("click", () => {
     navList.classList.toggle("show");
+    menuIcon.classList.toggle("close-icon");
 })
